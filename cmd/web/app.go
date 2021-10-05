9a73c60f2ac6b433886fc185147ea3032e255816
Chris James <quii@hey.com>
2021-10-05 16:36:10 +0100

    get a handle on cancellation

--- cmd/web/app.go
+++ cmd/web/app.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"context"
 	"github.com/quii/go-http-reference-impl/internal/adapters/http"
 	in_mem "github.com/quii/go-http-reference-impl/internal/adapters/in-mem"
 	"github.com/quii/go-http-reference-impl/internal/application/greet"
 	"github.com/quii/go-http-reference-impl/internal/ports"
+	"log"
 )
 
 // App holds and creates dependencies for your application
@@ -14,11 +16,23 @@ type App struct {
 	RecipeService ports.RecipeService
 }
 
-func newApp() *App {
+func newApp(applicationContext context.Context) *App {
 	config := newDefaultConfig()
+
+	go doSomethingOnInterrupt(applicationContext)
+
 	return &App{
 		ServerConfig:  config,
 		Greeter:       ports.GreeterServiceFunc(greet.HelloGreeter),
 		RecipeService: in_mem.NewRecipeStore(),
 	}
+}
+
+// this is just an example of how the services within an app could listen to the
+// cancellation signal and stop their work gracefully. So it's probably a decent
+// idea to pass the application context to services if you want to do some
+// cleanup before finishing
+func doSomethingOnInterrupt(ctx context.Context) {
+	<-ctx.Done()
+	log.Println("☠️ Program has been told to quit, I should tidy things up ☠️")
 }
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,12 +1,38 @@
 package main
 
 import (
+	"context"
 	"github.com/quii/go-http-reference-impl/internal/adapters/http"
 	"log"
+	"os"
+	"os/signal"
+)
+
+const (
+	exitCodeInterrupt = 2
 )
 
 func main() {
-	app := newApp()
+	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	defer func() {
+		signal.Stop(signalChan)
+		cancel()
+	}()
+
+	go func() {
+		select {
+		case <-signalChan: // first signal, cancel context
+			cancel()
+		case <-ctx.Done():
+		}
+		<-signalChan // second signal, hard exit
+		os.Exit(exitCodeInterrupt)
+	}()
+
+	app := newApp(ctx)
 
 	server := http.NewWebServer(
 		app.ServerConfig,
