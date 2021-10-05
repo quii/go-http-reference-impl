2c13d38a6e6cd049c0004e509206d7290a8bb1fd
Chris James <quii@hey.com>
2021-10-05 16:49:11 +0100

    tidy cancellation

---
+++ cmd/web/cancellation.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"context"
+	"os"
+	"os/signal"
+)
+const (
+	exitCodeInterrupt = 2
+)
+
+//https://pace.dev/blog/2020/02/17/repond-to-ctrl-c-interrupt-signals-gracefully-with-context-in-golang-by-mat-ryer.html
+// this creates an application context which will emit a cancel signal when an interrupt it sent or the application ends
+func listenForCancellationAndAddToContext() (ctx context.Context, done func()) {
+	ctx, cancel := context.WithCancel(context.Background())
+
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+
+	done = func() {
+		signal.Stop(signalChan)
+		cancel()
+	}
+
+	go func() {
+		select {
+		case <-signalChan: // first signal, cancel context
+			cancel()
+		case <-ctx.Done():
+		}
+		<-signalChan // second signal, hard exit
+		os.Exit(exitCodeInterrupt)
+	}()
+
+	return ctx, done
+}
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,36 +1,13 @@
 package main
 
 import (
-	"context"
 	"github.com/quii/go-http-reference-impl/internal/adapters/http"
 	"log"
-	"os"
-	"os/signal"
-)
-
-const (
-	exitCodeInterrupt = 2
 )
 
 func main() {
-	ctx := context.Background()
-	ctx, cancel := context.WithCancel(ctx)
-	signalChan := make(chan os.Signal, 1)
-	signal.Notify(signalChan, os.Interrupt)
-	defer func() {
-		signal.Stop(signalChan)
-		cancel()
-	}()
-
-	go func() {
-		select {
-		case <-signalChan: // first signal, cancel context
-			cancel()
-		case <-ctx.Done():
-		}
-		<-signalChan // second signal, hard exit
-		os.Exit(exitCodeInterrupt)
-	}()
+	ctx, done := listenForCancellationAndAddToContext()
+	defer done()
 
 	app := newApp(ctx)
 
